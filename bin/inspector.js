e2592cf89013e1634ed050867975013661081974
Tomasz Janczuk <tomasz@janczuk.org>
2011-10-21 12:38:58 -0700

    add support for xhr-polling transport

--- bin/inspector.js
+++ bin/inspector.js
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
 
+if (process.env.SystemDrive) { // we are running on Windows
+  // This is a "nuclear option" workaround for a node Windows issue https://github.com/joyent/node/issues/1697
+  // An intermittent assert in net_uv is causing the process to go down which reduces
+  // the reliability of the end to end debugging experience. This problem manifests itself
+  // frequently with xhr-polling transport. The bug is expected to be fixed in v0.6.0 of node.
+
+  process.on('uncaughtException', function (err) {
+    console.log('Unandled exception cought: ' + err);
+    console.log(err.stack);
+    console.stdout.flush();
+    console.stderr.flush();
+  });
+}
+
 var DebugServer = require('../lib/debug-server').DebugServer,
     fs = require('fs'),
     path = require('path'),
@@ -43,15 +57,11 @@ fs.readFile(path.join(__dirname, '../config.json'), function(err, data) {
       });
     }
   }
-  if (!config.webPort) {
-    config.webPort = 8080;
-  }
-  if (!config.debugPort) {
-    config.debugPort = 5858;
-  }
-  if (options.webPort) {
-    config.webPort = options.webPort;
-  }
+  // process.env.PORT, if specified, will take precedence over the configured web port
+  // NOTE: it need not be TCP port number, it may be a string file descriptor e.g. named pipe name
+  config.webPort = process.env.PORT || options.webPort || config.webPort || 8080;
+  config.debugPort = process.env.DEBUGPORT || config.debugPort || 5858;
+  config.transports = config.transports || ['websocket'];
 
   debugServer = new DebugServer();
   debugServer.on('close', function () {
